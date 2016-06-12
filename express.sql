4149f9e7fe3932ed646beaad482d7f15a58e516c
huangkaikai412 <934157585@qq.com>
2016-06-13 02:40:08 +0800

    userinfo

--- express.sql
+++ express.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- 主机: localhost
--- 生成日期: 2016-06-12 23:56:35
+-- 生成日期: 2016-06-13 02:39:10
 -- 服务器版本: 5.5.49-0ubuntu0.14.04.1
 -- PHP 版本: 5.5.9-1ubuntu4.16
 
@@ -29,6 +29,8 @@ SET time_zone = "+00:00";
 CREATE TABLE IF NOT EXISTS `list` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `req_ID` char(50) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
+  `nickname` varchar(20) NOT NULL,
+  `img` text NOT NULL,
   `res_ID` char(50) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
   `usrname` varchar(50) CHARACTER SET gb2312 NOT NULL,
   `address` varchar(10) CHARACTER SET gb2312 NOT NULL,
@@ -38,17 +40,14 @@ CREATE TABLE IF NOT EXISTS `list` (
   `state` int(11) NOT NULL,
   `time` datetime NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=gbk AUTO_INCREMENT=5 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=gbk AUTO_INCREMENT=2 ;
 
 --
 -- 转存表中的数据 `list`
 --
 
-INSERT INTO `list` (`id`, `req_ID`, `res_ID`, `usrname`, `address`, `company`, `telephone`, `res_tele`, `state`, `time`) VALUES
-(1, '', '', '黄凯凯', '13号楼E1313', '韵达', 18668775393, 0, 1, '0000-00-00 00:00:00'),
-(2, '', '', '孙晴', '13号楼', '顺丰', 12345678911, 0, 1, '0000-00-00 00:00:00'),
-(3, '', '', '孙晴', '13号楼', '顺丰', 12345678911, 0, 1, '0000-00-00 00:00:00'),
-(4, '', '', '黄凯凯', '13号楼E1313', '申通', 18668775393, 0, 1, '0000-00-00 00:00:00');
+INSERT INTO `list` (`id`, `req_ID`, `nickname`, `img`, `res_ID`, `usrname`, `address`, `company`, `telephone`, `res_tele`, `state`, `time`) VALUES
+(1, 'ophb9vvuOS6qXVJTlwfRwuQFvAdU', '黄凯凯', 'http://wx.qlogo.cn/mmopen/rqvn1hjHytfQdosEuKpczibJp1et5xpuO8DsjHXfjpfLjbqriaA5Th5L3GaguPD2ictNguNehrCdb5xykNTG3sTUw/0', NULL, '黄凯凯', '13号楼E1313', '中通', 18668775393, NULL, 1, '2016-06-13 02:27:43');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
